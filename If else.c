49797a28541b5017c07c6cbf157cef98f3ac14eb
Chandanasb123 <c2401022@gmail.com>
2024-11-29 18:38:41 +0530

    Create If else.c

---
+++ If else.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+int (num>0) {
+printf("enter the number");
+scanf("%d",&n!);
+printf("enter the number is even");
+else}
+printf("enter the number is odd");
+else}
+return 0;
+}
