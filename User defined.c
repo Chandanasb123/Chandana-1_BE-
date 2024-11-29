797a258d0681bfa3e0e2c4f4b87dd4afc7064ded
Chandanasb123 <c2401022@gmail.com>
2024-11-29 19:20:06 +0530

    Create User defined.c

---
+++ User defined.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int add(int a, int b);
+
+int main() {
+    int num1, num2, result;
+
+  
+    printf("Enter two numbers);
+    scanf("%d %d", &num1, &num2);
+
+
+    result = add(num1, num2);
+
+  
+    printf("The sum of %d and %d is: %d\n", num1, num2, result);
+
+    return 0;
+}
+
+int add(int a, int b) {
+    return a + b;  // Return the sum of a and b
+}
